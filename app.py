f4d4971a2472a6e1df0c33ac5fa04a7b45a18bf1
Vatsal Krishna <vatsalkrishna@outlook.com>
2014-08-02 15:13:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask, render_template, request, redirect, g  
+from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
+from flask.ext.login import LoginManager
+
+app = Flask("FlaskLoginApp")
+app.config['SECRET_KEY'] = '123456790'
+
+
+app.config['MONGODB_SETTINGS'] = {'HOST':os.environ.get('MONGOLAB_URI'),'DB': 'FlaskLogin'}
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.debug = os.environ.get('DEBUG',False)
+
+db = MongoEngine(app) 
+app.session_interface = MongoEngineSessionInterface(db) 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
