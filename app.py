dc7a0ab2e22764ac43d0529dc59b62d29d4527a1
Vatsal Krishna <vatsalkrishna@outlook.com>
2014-08-21 01:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask.ext.login import LoginManager
 
 
-app = Flask("dbmsApp")
+app = Flask("dbmsApp")  #create and name the flask project
 app.config['SECRET_KEY'] = '123456790'
 
-
+# for database connections
 app.config['MONGODB_SETTINGS'] = {'HOST':os.environ.get('MONGOLAB_URI'),'DB': 'FlaskLogin'}
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.debug = os.environ.get('DEBUG',False)
 
-db = MongoEngine(app) 
-app.session_interface = MongoEngineSessionInterface(db) 
+
+db = MongoEngine(app)  #connecting mongo engine with flask app
+app.session_interface = MongoEngineSessionInterface(db)  #for sessins with mongoengine
 
 login_manager = LoginManager()
 
-login_manager.init_app(app)
+login_manager.init_app(app)  #associate flask login manager with current app
