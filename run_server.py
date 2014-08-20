f79b7bba3a11bca0f34c25ce6006ecd3ad36f077
Vatsal Krishna <vatsalkrishna@outlook.com>
2014-08-21 02:25:22 +0530

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,11 +1,11 @@
 import os
 
-from app import app
-
-from fileupload import file_upload
+from app import app                                #import the main flask app
+ 
+from fileupload import file_upload                   #get your blueprints
 from auth import auth_flask_login
 
-app.register_blueprint(file_upload)
+app.register_blueprint(file_upload)                #register your blueprint
 app.register_blueprint(auth_flask_login)
 
 if __name__ == "__main__":
